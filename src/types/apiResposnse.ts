86876b288da830f072f7de9c8609b2cfbeb87b23
Kushagra <kushagrasharma826@gmail.com>
2026-09-24 19:14:11 +0530

    Revert "converted authcontext to ts"
    This reverts commit 62e724d4e64d2ad2a709fc88cfa375d4c67fa124.

--- src/types/apiResposnse.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./user";
-
-export interface LoginResponse {
-  user: User;
-}
-
-export interface SignupResponse {
-  user: User;
-}
--- src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./user";
-export interface AuthResponse {
-  user: User | null;
-  loggedIn: boolean;
-  login: (user: User) => void;
-  logout: () => void;
-    
-}
-
